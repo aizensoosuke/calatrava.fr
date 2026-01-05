cd1c02c30a65660d73d083cfd128bb0e402340e5
Sōsuke Aizen <yohan@abehssera.com>
2026-01-05 22:30:08 +0100

    feat: email customer on order or payment failure

---
+++ app/Mail/OrderPlaced.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Lunar\Models\Order;
+
+class OrderPlaced extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Order $order;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Order $order)
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: "Nouvelle commande #{$this->order->reference}",
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'mail.order-placed',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [
+            Attachment::fromData(function () {
+                return Pdf::loadView('lunarpanel::pdf.order', [
+                    'record' => $this->order,
+                ])->output();
+            }, "facture-{$this->order->reference}.pdf")
+        ];
+    }
+}
