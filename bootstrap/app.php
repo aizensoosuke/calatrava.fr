c4ef0707da89b602d717a5f9295f98848ff36426
Sōsuke Aizen <yohan@abehssera.com>
2026-01-06 15:13:11 +0100

    fix: trust proxies in prod

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,18 +14,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        if (env('APP_ENV') === 'local') {
-            $middleware->trustProxies('*');
-            $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
-                Request::HEADER_X_FORWARDED_HOST |
-                Request::HEADER_X_FORWARDED_PORT |
-                Request::HEADER_X_FORWARDED_PROTO |
-                Request::HEADER_X_FORWARDED_AWS_ELB
-            );
-            $middleware->alias([
-                'disabled' => Disabled::class,
-            ]);
-        }
+        $middleware->trustProxies('*');
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+        $middleware->alias([
+            'disabled' => Disabled::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
