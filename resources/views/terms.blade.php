9d892d893e4c1a1a5e4b184811619f37cdd0246a
Sōsuke Aizen <yohan@abehssera.com>
2026-09-02 20:15:29 +0200

    chore: legalese

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,9 +1,117 @@
 <x-layouts.app>
-    <div class="w-full px-8 lg:px-16 py-4 max-w-4xl mx-auto space-y-4 shadow">
+    <div class="w-full !my-8 px-8 lg:px-16 py-12 max-w-4xl mx-auto space-y-4 shadow">
         <h1 class="text-xl text-gray-900 font-bold">
             Conditions générales de vente
         </h1>
 
-        Conditions générales de vente en construction.
+        <p>
+            Heavenly, Mathilde Poquet<br />
+            13 rue du Pont,<br />
+            25420 Voujeaucourt<br />
+            contact@calatrava.fr
+        </p>
+        <p>
+            Les présentes conditions générales de vente régissent la relation entre la boutique Heavenly représentée par
+            Mathilde Poquet et l’acheteur ou utilisateur. Elles sont accessibles en permanence sur calatrava.fr
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-1-champ-dapplication-des-conditions-générales-de-vente">
+            ARTICLE 1 : CHAMP D’APPLICATION DES CONDITIONS GÉNÉRALES DE VENTE
+        </h2>
+        <p>
+            Les présentes conditions générales de vente s’appliquent à toutes commandes passées sur le site internet
+            calatrava.fr
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-2-acceptation-des-conditions-générales-de-vente">
+            ARTICLE 2 : ACCEPTATION DES CONDITIONS GÉNÉRALES DE VENTE
+        </h2>
+        <p>
+            Lors d’une commande en ligne, le consommateur coche la case « Accepter les conditions générales de vente », cela
+            constitue une acceptation irrévocable des présentes conditions générales de vente.
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-3-les-produits-et-les-prix">ARTICLE 3 : LES PRODUITS ET LES PRIX</h2>
+        <p>
+            Les offres de produits et de prix sont valables tant qu’elles sont visibles sur le site, dans la limite des stocks
+            disponibles.<br />
+            Calatrava by Heavenly se réserve le droit de modifier ses prix à tout moment et s’engage à appliquer les tarifs en
+            vigueur qui vous aurons été indiqué au moment de votre commande.<br />
+            Les commandes sont facturées et payables uniquement en euros.<br />
+            Les produits demeurent la propriété de Heavenly jusqu’au complet encaissement de la commande. Toutefois à compter de
+            la réception de la commande par le client, les risques des marchandises livrées sont transférés au client.
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-4-expédition-livraison-droit-de-rétractation-et-frais-de-port">
+            ARTICLE 4 : EXPÉDITION, LIVRAISON, DROIT DE RÉTRACTATION ET FRAIS DE PORT
+        </h2>
+        <p>
+            Les produits sont livrés à l’adresse de livraison indiquée au cours du processus de commande.<br />
+            Votre commande vous parviendra dans un délai de 7 jours, comprenant un délai d’expédition qui englobe la préparation
+            de votre commande et son emballage ainsi qu’un délai de livraison variable selon la destination.<br />
+            Vous trouverez le montant des frais de livraison correspondant à votre commande sur la facture qui vous est adressée
+            par courriel.<br />
+            Conformément à l’Article L.221-5, vous disposez d’un délai de 14 jours francs pour exercer votre droit de
+            rétractation sans avoir à justifier de motifs ni à payer de pénalités, à l’exception, des frais de retour qui reste
+            à votre charge. Ce délai mentionné court à compter de la réception du colis par le client.
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-5-paiement">ARTICLE 5 : PAIEMENT</h2>
+        <p>
+            Vous pouvez effectuer votre paiement par carte bancaire.<br />
+            Votre commande ne sera traitée qu’après réception et validation du paiement.
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-6-responsabilités-litiges">ARTICLE 6 : RESPONSABILITÉS / LITIGES</h2>
+        <p>
+            Heavenly ou son propriétaire ne saurait être tenu pour responsable de l’inexécution du contrat conclue, en cas de
+            force majeure et notamment en cas grève des services postaux, de transporteurs, et de catastrophes causées par des
+            inondations ou incendies, des moyens de communication.<br />
+            Heavenly ne peut être tenu pour responsable des dommages de toute nature, tant matériels qu’immatériels ou
+            corporels, qui pourraient résulter d’un mauvais fonctionnement ou de la mauvaise utilisation des produits
+            commercialisés.<br />
+            La responsabilité d’Heavenly ou de son propriétaire ne saurait également être engagée par un des utilisateurs ou
+            acheteur du site calatrava.fr en raison soit de la nature des informations, des produits vendus, des conseils
+            donnés ou des photographies illustrant les produits. Nous ne pouvons garantir l’exactitude, l’exhaustivité et la
+            mise à jour des informations diffusées sur le site. Nous déclinons, en conséquence, toutes responsabilités. Vous
+            pouvez néanmoins, si l’information vous parait incomplète ou erronée nous le signaler par mail :
+            contact@calatrava.fr<br />
+            Par ailleurs, Heavenly ou son propriétaire ne pourra être tenu pour responsable pour le non respect des dispositions
+            règlementaires et législatives en vigueur dans le pays de réception.
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-7-réclamations-retour-de-la-marchandise-et-droit-de-rétractation">
+            ARTICLE 7 : RÉCLAMATIONS, RETOUR DE LA MARCHANDISE ET DROIT DE RÉTRACTATION
+        </h2>
+        <p>
+            Vous devez impérativement vérifier la conformité de la marchandise livrée au moment de la livraison.<br />
+            Si les produits nécessitent de nous être renvoyés, ils doivent faire l’objet d’une demande préalable de retour
+            auprès de notre Service Client :<br />
+            Directement par le formulaire du site ou contact@calatrava.fr ou par courrier, dans un délai de 14 jours après la
+            livraison pour une rétractation : pour retourner tout article ne vous convenant pas ou le remboursement sans
+            pénalité, à l’exception des frais de retour et ce conformément à l’Article L.121-20 et suivant le code de la
+            consommation.<br />
+            dans les 15 jours pour défectuosité ou si vous n’avez pas reçu la bonne référence.<br />
+            après manifestation du client et vérification du vice caché.<br />
+            Les réclamations formulées hors de ces délais ne pourront être acceptées. Seuls les produits dans leur état
+            d’origine (emballage, notice et accessoire) pourront faire l’objet d’une rétractation. Le(s) produit(s) devra (ont)
+            être obligatoirement retourné(s) en parfait état, dans son (leurs) emballage(s) d’origine non endommagé(s). A défaut
+            vous perdrez votre droit de rétractation et le(s) produit(s) vous sera (ont) retourné(s) à vos frais.<br />
+            Après réception et acceptation du retour, nous procèderont au remboursement du ou des articles sous 15 jours. Les
+            frais de retours sont à la charge du client. Heavenly se réserve le droit de refuser toute commande pour des motifs
+            légitimes, notamment le défaut de paiement, adresse erronée ou autre problème sur le compte du client et plus
+            particulièrement si les quantités de produits commandés sont anormalement élevées pour des clients ayant la qualité
+            de consommateurs.
+        </p>
+        <p>
+            Le contrat de vente est formé au moment de l’envoi par le client de la confirmation de sa commande. Une fois
+            confirmée et acceptée par le Vendeur, dans les conditions ci-dessus décrites, la commande ne peut être annulée pour
+            le client, hors l’exercice du droit de rétractation ou cas de force majeure.
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-8-garanties">ARTICLE 8 : GARANTIES</h2>
+        <p>
+            Nous vous rappelons que vous bénéficiez de la garantie légale des vices cachés en applications des articles 1641 et
+            suivants du code civile qui vous garantit contre tous les défauts et vices cachés de la marchandise.
+        </p>
+        <h2 class="text-xl text-gray-900" id="article-9-loi-applicable">ARTICLE 9 : LOI APPLICABLE</h2>
+        <p>
+            Les présentes conditions générales sont soumises à l’application du droit français, à l’exclusion des dispositions
+            de la convention de Vienne. Il en est ainsi pour les règles de fond comme pour les règles de forme. En cas de litige
+            ou de réclamation, l’acheteur s’adressera en priorité au vendeur pour obtenir une solution amiable.
+        </p>
+
     </div>
 </x-layouts.app>
